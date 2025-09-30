bf123b1e8b91664038177a8747975958ca739b00
Alexsandro Gomes Clemente <alexsandro.gomes.clemente@hotmail.com>
2025-09-30 17:05:49 -0300

    Ajustes no prisma

--- back-end/src/controllers/likesController.ts
+++ back-end/src/controllers/likesController.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import prisma from "../config/prisma.config";
 
 export const createLike = async (req: Request, res: Response) => {
-  const { userId, targetId } = req.body;
+  const { userId, targetId, like } = req.body;
+
+  console.log(userId, "userId");
+  console.log(targetId, "TargeId");
   try {
-    const like = await prisma.like.create({
-      data: { userId, targetId },
+    const likes = await prisma.like.create({
+      data: { userId, targetId, like },
     });
-    return res.json(like);
+    return res.json(likes);
   } catch (err: any) {
     return res.status(400).json({ error: err.message });
   }
